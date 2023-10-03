08f683470768fb8da0a1c4d210c9f8cf55f53703
João Paulo Hora <joaopaulo8hora@gmail.com>
2023-10-03 04:30:54 -0300

    includes menu, btn-for-top and changes on footer

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ function sendForm() {
       return false; 
     }
   }
+
+  const list = document.querySelectorAll('.list');
+  function activeLink() {
+    list.forEach((item) => 
+    item.classList.remove('active'));
+    this.classList.add('active');
+  }
+
+    list.forEach((item) => 
+    item.addEventListener('click', activeLink));
+
+    const button = document.querySelector(".btn-for-top");
+
+  window.addEventListener("scroll", function (event) {
+    if (window.scrollY <= 165) {
+      button.classList.remove("visible");
+    } else {
+      button.classList.add("visible");
+    }
+  });c
